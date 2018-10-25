cb86dffc35f7b3e61128228a312c23e81e9b440f
Anatolii Zhloba <anatolyjloba@gmail.com>
2018-10-25 14:31:23 +0300

    Update .travis/deploy_heroku.sh

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
+heroku container:login
 heroku container:push web --app terlins/cicd-buzz
