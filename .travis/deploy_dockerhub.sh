d8b72a10ad69565e1bd254ec505d3d7750d6d326
Anatolii Zhloba <anatolyjloba@gmail.com>
2018-10-25 12:27:23 +0300

    docker2

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    TAG="latest"
+if [ "$TRAVIS_BRANCH" = "master" ];
 else
     TAG="$TRAVIS_BRANCH"
 fi
