b119564b2b940f0502c15da59030cf9d1f84af3c
Anatolii Zhloba <anatolyjloba@gmail.com>
2018-10-25 13:27:04 +0300

    Update .travis/deploy_heroku.sh

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-heroku login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
+heroku plugins:install heroku-container-registry
+heroku login -e _ -u _ --password=bd88dab9-8449-42ae-85ce-ae018dd72710 registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
