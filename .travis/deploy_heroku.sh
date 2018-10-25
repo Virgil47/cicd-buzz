17c615dc76b1497fda773f18728daff3b6e7c280
Anatolii Zhloba <anatolyjloba@gmail.com>
2018-10-25 14:40:43 +0300

    Update .travis/deploy_heroku.sh

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku container:login
-heroku container:push web --app terlins/cicd-buzz
+# heroku container:push web --app terlins/cicd-buzz
